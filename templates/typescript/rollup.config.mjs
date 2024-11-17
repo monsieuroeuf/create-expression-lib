d2fbbc10588e9e721dde769d62d8a711d082e464
Ian Haigh <ian@ketchup.net.au>
2024-11-18 08:56:11 +1100

    refactor: update rollup config for improved readability and consistency

--- templates/typescript/rollup.config.mjs
+++ templates/typescript/rollup.config.mjs
@@ -1,30 +1,29 @@
-import typescript from '@rollup/plugin-typescript';
-import replace from '@rollup/plugin-replace';
-import afterEffectJsx from 'rollup-plugin-ae-jsx';
-import pkg from './package.json';
+import typescript from "@rollup/plugin-typescript";
+import replace from "@rollup/plugin-replace";
+import afterEffectJsx from "rollup-plugin-ae-jsx";
+import pkg from "./package.json" with { type: "json" };
 
 export default {
-  input: 'src/index.ts',
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    format: 'es',
-  },
-  external: Object.keys(pkg.dependencies),
-  plugins: [
-    replace({
-      preventAssignment: true,
-      values: {
-        _npmVersion: pkg.version,
-      },
-    }),
-    typescript({
-      module: 'esnext',
-      target: 'esnext',
-      noImplicitAny: true,
-      moduleResolution: 'node',
-      strict: true,
-    }),
-    afterEffectJsx(),
-  ],
+	input: "src/index.ts",
+	output: {
+		file: pkg.main,
+		format: "es",
+	},
+	external: Object.keys(pkg.dependencies),
+	plugins: [
+		replace({
+			preventAssignment: true,
+			values: {
+				_npmVersion: pkg.version,
+			},
+		}),
+		typescript({
+			module: "esnext",
+			target: "esnext",
+			noImplicitAny: true,
+			moduleResolution: "node",
+			strict: true,
+		}),
+		afterEffectJsx(),
+	],
 };
